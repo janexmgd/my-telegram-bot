ec6b0fb435075bbb62399cfbefa91fe0c8ad52b4
janexmgd <janexmgd@gmail.com>
2023-09-11 14:27:23 +0700

    adding regex from catch url

--- src/command/index.js
+++ src/command/index.js
@@ -2,13 +2,15 @@
 import scraper from '../helper/scraper.js';
 import axios from 'axios';
 import bot from '../app/bot.js';
+const regexLink =
+  /(https:\/\/www\.tiktok\.com\/@[\w.-]+\/video\/\d+|https:\/\/vt\.tiktok\.com\/[\w.-]+)/g;
+
 const command = async (reqBody) => {
   try {
     // middleware untuk nangkap ttdl
     // bot.handleUpdate(reqBody);
     bot.use(async (ctx, next) => {
       const messageText = ctx.message.text;
-
       if (messageText.startsWith('/tiktokdl')) {
         const commandParts = messageText.split(' ');
 
@@ -28,8 +30,22 @@ const command = async (reqBody) => {
             await ctx.replyWithVideo({ source: response.data });
             ctx.reply('success download video');
           } catch (error) {
-            console.error('Gagal mengunduh video:', error);
+            ctx.reply(error.message);
           }
+        }
+      }
+      if (messageText.match(regexLink)) {
+        try {
+          ctx.reply('please waiting');
+          const p = await scraper(urlTikTok);
+          const videoUrl = p.data.url;
+          const response = await axios.get(videoUrl, {
+            responseType: 'stream',
+          });
+          await ctx.replyWithVideo({ source: response.data });
+          ctx.reply('success download video');
+        } catch (error) {
+          ctx.reply(error.message);
         }
       }
       return;
