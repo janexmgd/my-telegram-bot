5eca99cfa9998da3a31c534a8f7a6d0ad316330d
janexmgd <janexmgd@gmail.com>
2023-10-16 14:26:42 +0700

    add url param to function instaDL

--- src/helper/instaDL.js
+++ src/helper/instaDL.js
@@ -1,6 +1,6 @@
 import IgDL from '@sasmeee/igdl';
 // import IgDL from 'instagram-url-direct';
-const instaDL = async () => {
+const instaDL = async (url) => {
   try {
     // const url =
     //   'https://www.instagram.com/p/CybJZkLLWur/?utm_source=ig_web_copy_link';
